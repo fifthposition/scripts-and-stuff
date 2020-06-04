5fc313470e53845ae9abb461e9eff49251fb247b
Ben Klein <bklein@pivotal.io>
2020-06-04 18:19:02 -0500

    Again

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export fly_target=${fly_target:-tutorial}
-echo "Concourse API target ${fly_target}"
-echo "Tutorial $(basename $DIR)"
+#DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+#export fly_target=${fly_target:-tutorial}
+#echo "Concourse API target ${fly_target}"
+#echo "Tutorial $(basename $DIR)"
 
 #pushd $DIR
 #  fly -t ${fly_target} execute -c task_hello_world.yml
